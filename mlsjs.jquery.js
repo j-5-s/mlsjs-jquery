82619db7322c4c8e7c191f308ac6abba587ae0de
James Charlesworth <james.charlesworth@gmail.com>
2012-03-24 11:08:33 -0400

    initial commit

---
+++ mlsjs.jquery.js
@@ -0,0 +1,515 @@
+// MLSjs Plugin
+// @param {Object} options (required if not initialized on the jQuery el)
+// @param {String} method_name see @available_methods
+// @param {Mixed} method_option see @available_methods
+// @returns {jQuery}
+// @example
+//	$(document).ready(function(){
+//		$('body').MLSjs({
+//			account_id: '1001',
+//			template: $('#property_template')
+//		},'fetchAndRenderProperty','4e9ac8e1a454386c01000283');
+//	});
+//	Remove the property and data
+//	$('body').MLSjs('destroy');
+
+/*globals jQuery */
+(function( $ ){
+	'use strict';
+
+	$.fn.MLSjs = function( options, method, args ) {
+		// Options object is being supplied
+
+		if (typeof options === 'object') {
+			options = $.extend( {
+				'el' : this,
+				'account_id': '1001',
+				'city': 'atlanta, ga'
+			}, options);
+			this.data('mlsjs',options);
+		} else {
+			args = method;
+			method = options;
+			options = this.data('mlsjs');
+			if (!options) {
+				$.error('MLSjs has not been initiated, please supply options');
+			}
+		}
+
+		var mlsjs =  new MLSjs( options );
+	
+		if (typeof method !== 'undefined')
+			mlsjs[method](args);
+		return this;
+	};
+
+
+	/**
+	* The public methods available on MLSjs
+	* @class
+	*/
+
+	var MLSjs = function( options ) {
+		this.mlsjs = new MLSjsUtility( options );
+		this.options = options;
+	};
+
+	/** @lends MLSjs */
+	(function(){
+
+		/**
+		* Destroys the MLSjs object from the DOM
+		* @function
+		*/
+		this.destroy = function() {
+			this.options.el
+			.empty()
+			.removeData();
+		};
+
+		/**
+		* Fetches the property from an id and 
+		* renders the template
+		* @function
+		* @param {String} id
+		* @returns {this} MLSjs
+		*/
+
+		this.fetchAndRenderProperty = function( options ) {
+			var self = this,
+				fn = options.success,
+				id;
+
+			if (typeof options === 'string') {
+				id = options;
+			} else {
+				id = options.id;
+			}
+
+			//check that id is a hash
+			//Assume that if the last part of the hash
+			//is /\w+, then its the id
+			//@example
+			// #show/4e9795ebed5c8baf0200001e or
+			// #property/4e9795ebed5c8baf0200001e
+			if (/\/(\w+)$/.test(id)) {
+				id = /\/(\w+)$/.exec(id)[1];
+			}
+
+
+			this.fetchProperty( {
+				id: id,
+				success: function( p ) {
+					self.mlsjs.renderPropertyTemplate( p );
+					fn.call(self.options.el,p);
+				}	
+			});
+			return this;
+		};
+
+		/**
+		* Gets the property data from the server
+		* or cached stored in $.data
+		* @function
+		* @param {Object} options
+		*     - id: {String} required
+		*     - success: {Function} required 
+		* @returns {Object} property
+		*/
+		this.fetchProperty = function( options ) {
+			var self = this,
+				id   = options.id,
+				fn   = options.success;
+
+			//check that id is a hash
+			if (/\/(\w+)$/.test(id)) {
+				id = /\/(\w+)$/.exec(id)[1];
+			}
+
+			// Check if the property data is stored on the el
+			// if so, use it	
+			if (this.options.el.data('mls-property-'+id)) {
+				fn(this.options.el.data('mls-property-'+id));
+			} else {	
+				this.mlsjs.fetchProperty( id, function( property ){
+					
+					self.options.el.data('mls-property-'+id,property);				
+					fn.call(self.options.el, property);
+				});
+			}
+		};		
+
+
+		/**
+		* Fetches the properties from an array of ids and 
+		* renders the template
+		* @function
+		* @param {Array} ids
+		* @returns {jQuery} this
+		*/
+
+		this.fetchAndRenderProperties = function( options ) {
+			var self = this,
+				ids;
+			if (typeof options === 'string') {
+				ids = [options];
+			} else {
+				ids = options.ids;
+			}
+
+			this.fetchProperties( {
+				ids: ids,
+				success: function( properties ) {
+					self.mlsjs.renderPropertiesTemplate( properties );
+				}	
+			});
+			return this;
+		};
+
+		/**
+		* Gets a list of properties by id's
+		* @param {Object} options
+		*      - ids: {Array} property_ids
+		*      - success: {Function} callback
+		*
+		* @function
+		* @returns {jQuery} this
+		*/
+
+		this.fetchProperties = function( options ) {
+			var self = this,
+				ids  = options.ids,
+				fn   = options.success;
+
+
+			this.mlsjs.fetchProperties( ids, function( properties ){
+
+				fn.call( self.options.el, properties );
+			});
+
+			return this;
+		};
+
+		/**
+		* Gets all the individual search fields available
+		* @function
+		* @returns {Array} search_fields
+		*/
+
+		this.getSearchFields = function( options ) {
+			var fn   = options.success,
+				self = this;
+
+				
+
+			this.mlsjs.getSearchFields( options, function( fields ){
+			
+				fn.call( self.options.el, fields );
+			});
+
+			return this;
+		};
+
+		/**
+		* Gets all the search fields and renders them in a form
+		* @function
+		* @param {Object} options
+		*      -    
+		* @returns {String} html
+		*/
+
+		this.renderSearchForm = function( options ) {
+	
+			var fn   = options.success,
+				self = this;
+
+				
+
+			this.mlsjs.getSearchFields( options, function( fields ){
+				self.mlsjs.renderSearchTemplate( fields );
+				if (fn)
+					fn.call( self.options.el );
+			});
+		};
+
+
+
+
+		/**
+		* Gets a list of properties based on a query
+		* @param {Object} options
+		*      - query_params: {Object} basic mongodb query parameter format
+		*      - success: {Function} callback(properties)
+		*
+		* @function
+		* @returns {jQuery} this
+		*/
+
+		this.queryProperties = function( options ) {
+			var self = this,
+				parameters  = options.parameters,
+				fn   = options.success;
+
+
+			this.mlsjs.queryProperties( parameters, function( properties, parameters ){
+			
+				fn.call( self.options.el, properties );
+			});
+		};	
+
+		/**
+		* Query the properties and render them to the jQuery el
+		* @function
+		* @param {Object} options
+		*      - parameters: query_parameters
+		*      - success: callback(properties)
+		* @returns {jQuery} 
+		*/
+
+		this.queryAndRenderProperties = function( options ) {
+			var self = this,
+				parameters  = options.parameters,
+				fn   = options.success;
+
+			console.log('tst')	
+			this.queryProperties({
+				parameters: parameters,
+				success: function( properties ) {
+					console.log(properties)
+					self.mlsjs.renderPropertiesTemplate( properties, parameters );
+					fn.call( self.options.el, properties );
+				}
+			});	
+			return this;
+		};
+
+		/**
+		* Query the properties and render them to the jQuery el
+		* @function
+		* @param {Object} options
+		*      - paramters: query_parameters
+		*      - success: callback(properties)
+		* @returns {jQuery} 
+		*/
+
+		this.queryAndRenderList = function( options ) {
+			var self        = this,
+				parameters  = options.parameters,
+				fn          = options.success;
+
+				//hash is optional
+				parameters.hash = parameters.hash || 'show';
+			
+			this.queryProperties({
+				parameters: parameters,
+				success: function( properties ) {
+					
+					self.mlsjs.renderListTemplate( properties, parameters );
+					fn.call( self.options.el, properties );
+				}
+			});	
+			return this;
+		};		
+
+		return this;
+	}).call( MLSjs.prototype );	
+
+	/* Private Methods */
+
+	/**
+	* @class
+	* @api Private
+	*/
+
+	var MLSjsUtility = function( options ) {
+		this.options = options;
+		this.url = 'http://api.localhost.james:3000';
+	};
+
+	/** @lends PrivateMLSjs */
+	(function(){
+
+		/**
+		* Fetch the property from the server
+		* @api private
+		* @function
+		* @param {String} id
+		* @param {Function} callback
+		*/
+		this.fetchProperty = function( id, fn ) {
+
+			$.getJSON( this.url+'/property/' + id + '?callback=?', function( data ){
+	
+				fn.call( null, data );
+			});
+		};
+
+		/**
+		* Fetch properties from the server
+		* @api private
+		* @function
+		* @param {Array} ids
+		* @param {Function} callback
+		*/
+		this.fetchProperties = function( ids, fn ) {
+			ids = $.map( ids, function( value, index ){
+				return ('id[]=' + value);
+			}).join('&');
+
+			$.getJSON( this.url+'/properties', ids ,function( data ){
+				fn.call( null, data );
+			});
+		};
+
+		/**
+		* Gets all the drop down data necessary for search
+		* @function
+		* 
+		*/
+		this.getSearchFields = function( options, fn ) {
+			var parameters  = options.parameters;
+
+			$.getJSON( this.url + '/fields/all' + '?callback=?', parameters, function( data ){
+				fn.call( null, data );
+			});
+		};
+
+		/**
+		* Queries properties
+		* @param {Object} params (mongodb like query params)
+		* @param {Function} callback(properties) - Array
+		* @function
+		*/
+		this.queryProperties = function ( params, fn ) {
+			var account_id = this.options.account_id;
+			$.getJSON( this.url+'/query/'+account_id + '?callback=?', params ,function( data ){
+			
+				fn.call( null, data.properties );
+			});
+		};
+
+
+		/**
+		* Renders the Property Template (Single Property)
+		* @function
+		* @param {Object} property
+		*/
+
+		this.renderPropertyTemplate = function( p ) {
+			this.options.el.html( this.getTemplate( 'property', p ) );
+			return this;
+		};
+
+		/**
+		* Renders the Properties Template (Multiple Properties)
+		* @function
+		* @param {Object} property
+		*/
+
+		this.renderPropertiesTemplate = function( properties ) {
+			this.options.el.html( this.getTemplate( 'properties', {properties:properties} ) );
+			return this;
+		};
+
+		/**
+		* Renders the List Template (Multiple Properties)
+		* @function
+		* @param {Object} property
+		*/
+
+		this.renderListTemplate = function( properties, parameters ) {
+			
+			var template = parameters.template || 'search_results';
+			
+			this.options.el.html( this.getTemplate( template, { properties:properties,parameters:parameters } ) );
+			return this;
+		};
+
+		/**
+		* Renders the search form
+		* @function
+		* @param {Object} fields
+		*/
+		this.renderSearchTemplate = function( fields ) {
+
+			this.options.el.html( this.getTemplate( 'search_form', { fields:fields } ) );
+			return this;			
+		};
+
+		/**
+		* Storage for templates
+		* @TODO place in separate file
+		*/
+		this.default_templates = {
+			'property'   : '<div><%= address %></div>',
+			'properties' : '<div><ul><% for ( var i = 0; i < properties.length; i++) { %><li><%= properties[i].address %></li><% } %></ul></div>',
+			'thumbnails' : '<ul class="MLSjs-list">'+
+					'<% for ( var i = 0; i < properties.length; i++) { %>' +
+					'<% if (properties[i].images.length > 0) { %>'+
+						'<li><a href="<%= parameters.property_page %>#<%= parameters.hash %>/<%= properties[i]._id %>"><img src="https://s3.amazonaws.com/mlsjs/uploads/<%= properties[i].images[0].filename %>" /></a></li>'+
+					'<% } %>'+
+					'<% } %></ul>',
+			'search_form': '<b>Search</b>',
+			'search_results' : 'hi'
+		};
+
+		/**
+		* Gets the html from a default template or
+		* a template provided via this.options.template
+		* @function
+		* @param {Object}
+		* @returns {String} html
+		*/
+
+		this.getTemplate = function( template_name, p ) {
+			var html = '';
+
+
+			if (typeof this.options.template !== 'undefined') {
+				html = this.options.template.html();
+			} else {
+
+				html = this.default_templates[template_name];
+		
+				//@TODO create html template
+			}
+
+			return this.template(html, p);
+		};
+		
+		/**
+		* Templating function.
+		* Based off of http://ejohn.org/blog/javascript-micro-templating/
+		* @param {String} html
+		* @param {Object} data	
+		* @returns {String} template
+		*/
+
+		this.template = function( str, data ) {
+			/*jshint evil:true */
+			// Generate a reusable function that will serve as a template
+			// generator (and which will be cached).
+			var fn = new Function("obj","var p=[],print=function(){p.push.apply(p,arguments);};" +
+
+			// Introduce the data as local variables using with(){}
+			"with(obj){p.push('" +
+
+			// Convert the template into pure JavaScript
+			str
+				.replace(/[\r\t\n]/g, " ")
+				.split("<%").join("\t")
+				.replace(/((^|%>)[^\t]*)'/g, "$1\r")
+				.replace(/\t=(.*?)%>/g, "',$1,'")
+				.split("\t").join("');")
+				.split("%>").join("p.push('")
+				.split("\r").join("\\'")+ "');}return p.join('');");
+
+			// Provide some basic currying to the user
+			return fn( data );
+		
+		};
+	}).call( MLSjsUtility.prototype );
+
+
+})( jQuery );
+
